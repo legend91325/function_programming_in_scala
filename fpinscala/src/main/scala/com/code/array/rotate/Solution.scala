1385bc750291b08b51eb72d0e930de737d5564df
WuKongCoder <213dormitory@gmail.com>
2023-03-23 00:05:03 +0800

    code dp

--- fpinscala/src/main/scala/com/code/array/rotate/Solution.scala
+++ fpinscala/src/main/scala/com/code/array/rotate/Solution.scala
@@ -15,11 +15,36 @@ object Solution {
    * @param nums
    * @param k
    */
-  def rotate(nums: Array[Int], k: Int): Array[Int] = {
+  def rotate1(nums: Array[Int], k: Int): Array[Int] = {
     val rotateNum = k % nums.length
     if (rotateNum==0 || nums.length==1)
       nums
     else
       nums.drop(nums.length-rotateNum)++nums.take(nums.length-rotateNum)
+  }
+
+  def rotate(nums: Array[Int], k: Int): Array[Int] = {
+    val rotateNum = k % nums.length
+    if (rotateNum == 0 || nums.length == 1)
+      nums
+    else {
+      reverse(nums,0,nums.length-1)
+      reverse(nums,0,rotateNum-1)
+      reverse(nums,rotateNum,nums.length-1)
+      nums
+    }
+  }
+
+  def reverse(nums: Array[Int], start: Int,end:Int): Array[Int] = {
+    var i = start
+    var j = end
+    while (i<j){
+      val temp =nums(i)
+      nums(i) = nums(j)
+      nums(j) = temp
+      i += 1
+      j -= 1
+    }
+    nums
   }
 }
